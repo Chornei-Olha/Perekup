42e448f4caca9a53be3fa0267199a6fe8e75d59f
workProject085 <workProject085@gmail.com>
2025-05-05 19:13:32 +0300

    changed css

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <div className="relative pt-4 px-4 sm:px-16">
-      <div className="flex flex-col md:flex-row justify-between items-left sm:items-center">
+      <div className="flex flex-col md:flex-row justify-between items-start sm:items-center">
         {/* Logo */}
         <div className="flex flex-col mb-[25px] sm:mb-0">
           <Link href="/">
@@ -18,25 +18,31 @@ export default function Header() {
         </div>
 
         {/* Contact info */}
-        <div className="flex flex-col sm:flex-row items-left sm:items-center space-x-6">
+        <div className="flex flex-col sm:flex-row items-start sm:items-center space-y-2 sm:space-y-0 sm:space-x-6">
           <div className="flex items-center">
             <MapPin className="h-5 w-5 mr-2" />
-            <span className="font-['Open_Sans'] font-light font-sm text-white">
+            <span className="font-['Open_Sans'] font-light text-sm text-white">
               Украина
             </span>
           </div>
-          <div className="flex items-center">
+          <a
+            href="mailto:pekekuppro7@gmail.com"
+            className="flex items-center hover:underline"
+          >
             <Mail className="h-5 w-5 mr-2" />
-            <span className="font-['Open_Sans'] font-light font-sm text-white">
+            <span className="font-['Open_Sans'] font-light text-sm text-white">
               pekekuppro7@gmail.com
             </span>
-          </div>
-          <div className="flex items-center">
+          </a>
+          <a
+            href="tel:+380500441132"
+            className="flex items-center hover:underline"
+          >
             <Phone className="h-5 w-5 mr-2" />
-            <span className="font-['Open_Sans'] font-light font-sm text-white">
+            <span className="font-['Open_Sans'] font-light text-sm text-white">
               +38(050) 044-11-32
             </span>
-          </div>
+          </a>
         </div>
       </div>
     </div>
